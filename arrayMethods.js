9aeb3180e9f69fa577a27b36fb3f7e47abec5b32
Mehdi Safari <sa.mehdisafari@gmail.com>
2021-12-22 10:12:37 -0800

    done with 3 problems in array methods

--- arrayMethods.js
+++ arrayMethods.js
@@ -1,53 +1,20 @@
-/*
-    You can check your answers in this file
-    by console.logging the variables that are 
-    storing the results of your function calls.
-*/
-
 ////////// PROBLEM 1 //////////
-
-// Do not edit the code below.
 const mixedNumbers = [6,3,1,7,5,2,6,8,9,4,2,7,9,3,1,8,4,3];
-// Do not edit the code above.
 
-/*
-  Use the filter method on mixedNumbers to make a new array of just the even numbers.
-  The filter function takes a callback with the parameters
-  function(element, index, wholeArray){}  Function Form
-  (element, index, wholeArray)=>{}    Arrow Form
-*/
-
-// CODE HERE
-const evenNumbers // = mixedNumbers.filter(/* Provide Your Callback Here */)
-
+const evenNumbers = mixedNumbers.filter(num => num % 2 === 0)
+console.log(`Here are the even numbers: ${evenNumbers}`)
 
 
 ////////// PROBLEM 2 //////////
 
-// Do not edit the code below.
 const prices = [15.00, 23.00, 78.00, 34.00, 12.00, 86.00, 12.00, 79.00, 32.00];
-// Do not edit the code above.
 
-/*
-  Use the map method on the prices array to calculate a new array of post-tax prices.
-  Use a 7% tax rate.
-  Math reminder! To calculate the price after tax, multiply the price by 1 plus the taxRate as a decimal.
-  Example: if tax is 7%, the price afterTax could be calculated like this: afterTax = price * 1.07) 
-  The map function also takes a callback with the parameters
-  function(element, index, wholeArray){}  Function Form
-  (element, index, wholeArray)=>{}    Arrow Form
-*/
-
-// CODE HERE
-const postTaxPrices // = prices.map(/* Provide Your Callback Here );
-
+const postTaxPrices = prices.map(num => num * 1.07)
+console.log(`Here are the Post-Tax-Prices numbers: ${postTaxPrices}`)
 
 
 ////////// PROBLEM 3 //////////
-
-// Do not edit the code below.
 const populations = [8175133, 3792621, 2695598, 2100263];
-// Do not edit the code above.
 
 /*
   Use the reduce method to calculate the sum of all the populations in the array.
@@ -56,8 +23,8 @@ const populations = [8175133, 3792621, 2695598, 2100263];
   (runningTotal, curElement, curIndex, wholeArray)=>{} Arrow Form
 */
 
-// CODE HERE
-const totalPopulation //  = populations.reduce(/* Provide Your Callback Here */)
+const totalPopulation = populations.reduce((a, c) => a + c)
+console.log(`The total population is: ${totalPopulation}`)
 
 
 
@@ -82,7 +49,7 @@ const monstersInYourPocket = [{"monster":"Bulbabunny","CP":156},{"monster":"Bulb
 */
 
 // CODE HERE
-const myStrongest // = monstersInYourPocket.filter(/* Provide Your Callback Here */)
+
 
 
 
